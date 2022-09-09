8d5bd864dfe8715cc3c60fe10fc51737e09ef9d3
ucheujor9 <ucheujor9@gmail.com>
2022-09-09 09:12:34 -0700

    Commits on loop

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ *  main - prints all possible combinations of 2 two digits
+ *  ranging from 0 to 99
+ *  return: 0 (success)
+ */
+
+int main (void)
+{
+	int n, k;
+	
+	for (n = 0; n < 100; n++)
+	{
+		for (k = 0; k < 100; k++)
+		{
+			if (n < k)
+			{
+				putchar((n / 10) + 48);
+				putchar((n % 10) + 48);
+				putchar(' ');
+				putchar((k / 10) + 48);
+				putchar((k % 10) + 48);
+				if (n != 98 || k != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
