07794a1debe01093ef97257fea5233908e9ed699
ucheujor9 <ucheujor9@gmail.com>
2022-09-12 03:29:17 -0700

    corrections

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,14 +2,14 @@
 #include <unistd.h>
 
 /**
- * main entry point
+ * main - prints a line of string
  *
- * return end point
+ * Return: 1 success
  */
 
 int main(void)
 {
-	write(2,
-"and that piece of art is useful\" -Dora Korpar, 2015-10-19\n", 59);
-	return (0);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
+	return (1);
 }
