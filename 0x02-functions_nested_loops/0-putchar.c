b7d2e2434567789e2267df0f659527ffc3559eaa
ucheujor9 <ucheujor9@gmail.com>
2022-09-13 03:45:17 -0700

    putchar commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *  main - prints _putchar in new line
+ *
+ *  Return: 0 success
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Hello, this is my README on loops, functions. check it out.
