2d010de7d6f4036ef0b64c735f4d91a509fc6aa9
ucheujor9 <ucheujor9@gmail.com>
2022-09-16 06:02:24 -0700

    correction

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -15,7 +15,7 @@ void print_triangle(int size)
 	{
 		for (inc1 = 1; inc1 <= size; inc1++)
 		{
-			for (( inc2 = size - inc1); inc2 > 0; inc2--)
+			for ((inc2 = size - inc1); inc2 > 0; inc2--)
 			{
 				putchar(' ');
 			}
